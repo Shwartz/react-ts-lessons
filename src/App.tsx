1f82fbf79fcaecd9e4f3441847b0360d532fdb2f
andris.svarcs <andris.svarcs@yahoo.com>
2019-01-30 17:54:21 +0000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+interface IProps {
+
+}
+
+export class App extends React.Component<IProps> {
+    render() {
+        return (<div>This is app</div>)
+    }
+}
+
+export default App;
