bf7798498dcf228a7c419fb06b1640bd076d8cf0
andris.svarcs <andris.svarcs@yahoo.com>
2019-01-31 16:50:22 +0000

    Basic routing for different lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './Home';
+import Lesson1 from './lesson-1/Lesson1';
+import Lesson2 from './lesson-2/Lesson2';
 import './App.css';
 
 interface IProps {
@@ -7,7 +11,15 @@ interface IProps {
 
 export class App extends React.Component<IProps> {
     render() {
-        return (<div>This is app</div>)
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/lesson-1" component={Lesson1}/>
+                    <Route path="/lesson-2" component={Lesson2}/>
+                </div>
+            </Router>
+        )
     }
 }
 
---
+++ src/lesson-2/Lesson2.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface IProps {
+
+}
+
+export class Lesson2 extends React.Component<IProps> {
+
+    render() {
+        return (
+            <div>
+                <p>Lesson 2</p>
+            </div>
+        );
+    }
+}
+
+export default Lesson2;
