9864c0da5427843c4d6e59681cf44239fab03a5a
andris.svarcs <andris.svarcs@yahoo.com>
2019-02-02 09:34:38 +0000

    adding react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/lesson1">Lesson 1 - simple counter</Link></li>
+                        <li><Link to="/lesson-2">Lesson 2</Link></li>
+                    </ul>
+                </nav>
+                <section>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/lesson1" component={Lesson1}/>
+                    <Route path="/lesson-2" component={Lesson2}/>
+                </section>
+            </div>
+        </Router>
     );
   }
 }
