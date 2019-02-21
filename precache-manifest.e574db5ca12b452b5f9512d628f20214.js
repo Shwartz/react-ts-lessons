6ca9f7b02313d4fab9ded1a41f14728946e92efb
andris.svarcs <andris.svarcs@yahoo.com>
2019-02-21 21:43:19 +0000

    Updates

---
+++ precache-manifest.e574db5ca12b452b5f9512d628f20214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d0e4d397c5be13f1833",
+    "url": "/react-ts-lessons/static/js/runtime~main.6d0e4d39.js"
+  },
+  {
+    "revision": "3cc06705d2658afe4ddc",
+    "url": "/react-ts-lessons/static/js/main.3cc06705.chunk.js"
+  },
+  {
+    "revision": "cb49f5b1ead286db4915",
+    "url": "/react-ts-lessons/static/js/1.cb49f5b1.chunk.js"
+  },
+  {
+    "revision": "3cc06705d2658afe4ddc",
+    "url": "/react-ts-lessons/static/css/main.765fbbb7.chunk.css"
+  },
+  {
+    "revision": "ba1da1894af300ac30309e28509a07f9",
+    "url": "/react-ts-lessons/index.html"
+  }
+];
